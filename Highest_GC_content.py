cd1b8906381a0488c46f1cfea313bbd8c4e69cb5
Emma-MacK <Emma.MacKenzie2@addenbrookes.nhs.net>
2022-10-19 09:33:34 +0100

    create Highest_GC_content

---
+++ Highest_GC_content.py
@@ -0,0 +1,15 @@
+
+# from a fasta file, read in sequences, calculate GC%
+# return ID of sequence with highest GC content
+
+# read in modules
+
+# create functions
+# Read in fasta, separating when encountering a <
+# count total number of bases
+# count G and G bases in sequences, store based on ID
+
+# Maybe have for loop, going over given sequences
+# first sequence stored into objects
+# if next object has higher GC, replace original ID and GC
+# once through all sequences, return highest sequence.
