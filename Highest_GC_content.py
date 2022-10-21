bd5d9d9095bd9f733b55716a31f786ebf0b24472
Emma-MacK <Emma.MacKenzie2@addenbrookes.nhs.net>
2022-10-21 09:56:13 +0100

    begin calculating GC content from sample file

--- Highest_GC_content.py
+++ Highest_GC_content.py
@@ -1,12 +1,40 @@
 
 # from a fasta file, read in sequences, calculate GC%
 # return ID of sequence with highest GC content
-
+import sys
 # read in modules
 
-# create functions
-# Read in fasta, separating when encountering a <
-# count total number of bases
+
+# take the file to be read, and read it
+input_file =  sys.argv[1]
+file = open(input_file, "r").read()
+# print(file.read()) 
+
+# separate the file into objects where a > is present
+# the fist element is a blank space, so remove
+# the length of the fasta is calculated
+# All elements of the fasta file returned
+
+fasta_length = len(file)
+list_fasta = file.split(">")[1:fasta_length]
+# print(list_fasta)
+
+# create a loop, run over elements a split into ID and sequence
+
+for sequence in list_fasta:
+    ID = sequence.split()[0]
+    # print(ID)
+    bases = sequence.split()[1]
+    # print(bases)
+    # count total number of bases
+    length_bases = len(bases)
+    # print(length_bases)
+    # count the number of Cs and Gs 
+    number_CG = bases.count("C") + bases.count("G")
+    # print(number_CG)
+    GC_per = number_CG/length_bases
+    print(GC_per)
+    
 # count G and G bases in sequences, store based on ID
 
 # Maybe have for loop, going over given sequences
